bcdadce70612c3c04a306a4ac58f7842c58145ea
Robert <robert.rostomyan@gmail.com>
2019-05-19 11:08:17 +0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,135 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import UUID from "uuid";
+import PropTypes from "prop-types";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import { withStyles } from "@material-ui/core/styles";
+import ToDoItem from "./components/ToDoItem";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const styles = () => ({
+  app: {
+    textAlign: "center"
+  },
+  wrapper: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    padding: 15
+  }
+});
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: "",
+      list: []
+    };
+  }
+
+  handleAddItem = () => {
+    const { list, inputValue } = this.state;
+    const generatedId = UUID.v4();
+    if (inputValue.trim())
+      this.setState({
+        list: [...list, { id: generatedId, name: inputValue, checked: false }],
+        inputValue: ""
+      });
+  };
+
+  handleDeleteItem = itemId => {
+    const { list } = this.state;
+    const itemToDelete = list.find(item => item.id === itemId);
+    if (itemToDelete) {
+      const index = list.indexOf(itemToDelete);
+      this.setState({
+        list: [...list.slice(0, index), ...list.slice(index + 1)]
+      });
+    }
+  };
+
+  handleItemCheckChange = itemId => {
+    const { list } = this.state;
+    const itemToCheck = list.find(item => item.id === itemId);
+    if (itemToCheck) {
+      const index = list.indexOf(itemToCheck);
+      this.setState({
+        list: [
+          ...list.slice(0, index),
+          { ...itemToCheck, checked: !itemToCheck.checked },
+          ...list.slice(index + 1)
+        ]
+      });
+    }
+  };
+
+  handleInputChange = event => {
+    this.setState({
+      inputValue: event.target.value
+    });
+  };
+
+  renderItem = item => {
+    return (
+      <ToDoItem
+        key={item.id}
+        item={item}
+        onDelete={this.handleDeleteItem}
+        onCheckChange={this.handleItemCheckChange}
+      />
+    );
+  };
+
+  onKeyDown = event => {
+    switch (event.keyCode) {
+      case 13: {
+        this.handleAddItem();
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  };
+
+  inputProps = {
+    onKeyDown: this.onKeyDown
+  };
+
+  render() {
+    const { inputValue, list } = this.state;
+    const { classes } = this.props;
+    return (
+      <div className={classes.app}>
+        <div className={classes.wrapper}>
+          <TextField
+            id="outlined-dense"
+            label="Dense"
+            variant="outlined"
+            value={inputValue}
+            onChange={this.handleInputChange}
+            InputProps={this.inputProps}
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={this.handleAddItem}
+          >
+            Add to do
+          </Button>
+        </div>
+        {list.map(item => this.renderItem(item))}
+      </div>
+    );
+  }
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.shape({
+    app: PropTypes.string,
+    wrapper: PropTypes.string
+  })
+};
+
+export default withStyles(styles)(App);
---
+++ src/components/ToDoItem/index.js
@@ -0,0 +1,79 @@
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import Button from "@material-ui/core/Button";
+import { withStyles } from "@material-ui/core/styles";
+
+
+const styles = () => ({
+  root: {
+    textAlign: "center"
+  },
+  wrapper: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    padding: 15
+  },
+  checked: {
+    color: "red"
+  }
+});
+
+class ToDoItem extends React.Component {
+  handleDeleteItem = event => {
+    const {
+      onDelete,
+      item: { id }
+    } = this.props;
+    onDelete(id);
+    event.stopPropagation();
+  };
+
+  handleItemClick = () => {
+    const {
+      onCheckChange,
+      item: { id }
+    } = this.props;
+    onCheckChange(id);
+  };
+
+  render() {
+    const {
+      classes,
+      item: { name, checked }
+    } = this.props;
+    return (
+      <div onClick={this.handleItemClick} className={classes.root}>
+        <div
+          className={classNames(classes.wrapper, {
+            [classes.checked]: false
+          })}
+        >
+          {checked && <span>checked</span>}
+          {name}
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={this.handleDeleteItem}
+          >
+            Romove
+          </Button>
+        </div>
+      </div>
+    );
+  }
+}
+
+ToDoItem.propTypes = {
+  classes: PropTypes.shape({
+    root: PropTypes.string,
+    wrapper: PropTypes.string,
+    checked: PropTypes.string
+  }),
+  item: PropTypes.any.isRequired,
+  onDelete: PropTypes.func.isRequired,
+  onCheckChange: PropTypes.func.isRequired
+};
+
+export default withStyles(styles)(ToDoItem);
